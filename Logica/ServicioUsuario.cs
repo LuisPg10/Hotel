30e2d3a8db313d512f3f91083e79190861efe863
LuisAndres92 <andres_guardian@hotmail.es>
2022-11-11 14:03:48 -0500

    agregacion de metodos en la parte logica

--- Logica/ServicioUsuario.cs
+++ Logica/ServicioUsuario.cs
@@ -1,8 +1,61 @@
 ﻿using System;
+using Entidades;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace Logica
 {
     public class ServicioUsuario
     {
+        List<Usuario> listaUsuarios = new List<Usuario>();
+        
+        public void registrarUsuario(Usuario user)
+        {
+            listaUsuarios.Add(user);
+        }
+
+        public bool validarUsuario(string user, string pass)
+        {
+            bool validar = false;
+
+            foreach (Usuario usuario in listaUsuarios)
+            {
+                if (usuario.Username.Equals(user) && usuario.Password.Equals(pass))
+                {
+                    validar = true;
+                }
+            }
+
+            return validar;
+        }
+
+        public bool validarUsuario(string dato,int op)
+        {
+            bool validar = false;
+
+            foreach (Usuario usuario in listaUsuarios)
+            {
+                switch (op)
+                {
+                    case 1:
+                        if (usuario.Username.Equals(dato))
+                        {
+                            validar = true;
+                        }
+                        break;
+                    case 2:
+                        if (usuario.Correo.Equals(dato))
+                        {
+                            validar = true;
+                        }
+                        break;
+                }
+                
+            }
+
+            return validar;
+        }
+
+
     }
 }
