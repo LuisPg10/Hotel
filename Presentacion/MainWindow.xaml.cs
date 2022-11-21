c90eb7547701aadb9f347b2953f7a64aac7d36cd
LuisPg10 <dapin46@hotmail.com>
2022-11-21 06:58:09 -0500

    Implementando nuevo diseño para el proyecto

---
+++ Presentacion/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Runtime.InteropServices;
+using System.Windows.Interop;
+
+namespace Presentacion
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr SendMessage(IntPtr hWnd, int wMsg, int wParam, int lParam);
+        private void barraTitulo_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            WindowInteropHelper helper = new WindowInteropHelper(this);
+            SendMessage(helper.Handle, 161, 2, 0);
+        }
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void barraTitulo_MouseEnter(object sender, MouseEventArgs e)
+        {
+            MaxHeight = SystemParameters.MaximizedPrimaryScreenHeight;
+        }
+
+        private void btnMaximize_Click(object sender, RoutedEventArgs e)
+        {
+            if (WindowState == WindowState.Normal)
+                WindowState = WindowState.Maximized;
+            else WindowState = WindowState.Normal;
+        }
+
+        private void btnMinimize_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+    }
+}
---
+++ Presentacion/ViewModels/MainViewModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Presentacion.ViewModels
+{
+    public class MainViewModel: ViewModelBase
+    {
+        private ViewModelBase _currentChildView;
+
+        //Propiedades
+        public ViewModelBase CurrentChildView
+        {   
+            get 
+            { 
+                return _currentChildView; 
+            }
+            set
+            {
+                _currentChildView = value;
+                OnPropertyChange(nameof(CurrentChildView));
+            }
+        }
+
+        public ICommand ShowLoginCommand { get; }
+
+        public MainViewModel()
+        {
+            //Initializate commands
+            ShowLoginCommand = new ViewModelCommand(ExecuteShowLoginCommand);
+
+            //Default view
+            ExecuteShowLoginCommand(null);
+        }
+        private void ExecuteShowLoginCommand(object obj)
+        {
+            CurrentChildView = new LoginViewModel();
+        }
+    }
+}
