93eed9fdf88d6b8a5f9d32efc9eae99d539dc4d8
Nayid04 <ncastellar@unicesar.edu.co>
2022-11-22 23:04:24 -0500

    Actulizacion de parametros y agregacion de la entidad 'Comentario'

---
+++ Entidades/Comentario.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Comentario
+    {
+        public uint Id { get; set; }
+        public Usuario Usuario { get; set; }
+        public Habitacion Habitacion { get; set; }
+        public string Comentarios { get; set; }
+
+        public Comentario()
+        {
+        }
+
+        public Comentario(uint id, Usuario usuario, Habitacion habitacion, string comentarios)
+        {
+            Id = id;
+            Usuario = usuario;
+            Habitacion = habitacion;
+            Comentarios = comentarios;
+        }
+    }
+}
