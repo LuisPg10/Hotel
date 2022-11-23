42bc5f482144df4aff6db50220128ad9e1e46c2d
Nayid04 <ncastellar@unicesar.edu.co>
2022-11-22 23:05:20 -0500

    Agregacion de la capa 'Datos', la cual hace la conexion con la base de datos

---
+++ Datos/Conexion.cs
@@ -0,0 +1,47 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Datos
+{
+    public class Conexion
+    {
+        public static MySqlConnection conectar;
+
+        public MySqlConnection obtenerConexion()
+        {
+            try
+            {
+                conectar = new MySqlConnection("server = 127.0.0.1; database = bdhotel; Uid = root; pwd = ;");
+                conectar.Open();
+                Console.WriteLine("Conexion exitosa!");
+                return conectar;
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine($"Error {ex.Message}");
+            }
+            return conectar;
+        }
+
+        public void cerrarConexion()
+        {
+            try
+            {
+                conectar.Close();
+                Console.WriteLine("Conexion cerrada!");
+            }catch(Exception ex)
+            {
+                Console.WriteLine($"Error {ex.Message}");
+                conectar.Close();
+            }
+        }
+
+    }
+}
