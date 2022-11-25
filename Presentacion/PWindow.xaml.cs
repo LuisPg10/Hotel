1606a6ffaff9be7159d3c007c74cb9ce0f254175
LuisAndres92 <andres_guardian@hotmail.es>
2022-11-25 07:25:30 -0500

    Nuevos cambios y modificaciones graficas

--- Presentacion/PWindow.xaml.cs
+++ Presentacion/PWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace Presentacion
         {
             InitializeComponent();
         }
+
+        private void ContentControl_Scroll(object sender, System.Windows.Controls.Primitives.ScrollEventArgs e)
+        {
+
+        }
     }
 }
